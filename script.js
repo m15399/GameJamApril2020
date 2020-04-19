ac2b54645dbb05754a3d6d50454ea1636f872d2e
Mark Gardner <m15399@gmail.com>
2020-04-18 20:35:23 -0700

    Add a scripting system

---
+++ script.js
@@ -0,0 +1,63 @@
+
+// Very simple scripting system to get things to happen over time.
+//
+// Usage:
+//   const script = new Script();
+//   script.after(delayInFrames, function(){
+//       doSomething();
+//   }).after(nextDelayInFrames, function(){
+//       doSomethingElse();
+//   }).after(...)
+//
+//   // Then update the script every frame.
+//   script.update();
+//
+class Script {
+	constructor(){
+		this.steps = [];
+		this.loopBeginMark = 0;
+		this.currStep = 0;
+		this.currDelay = 0;
+	}
+
+	after(delay, func, id){
+		this.steps.push({delay: delay, func: func, id: id});
+		return this;
+	}
+
+	loopBegin(){
+		this.loopBeginMark = this.steps.length;
+		return this;
+	}
+
+	loop(){
+		this.steps.push({loop: true});
+		return this;
+	}
+
+	executeNextStep(){
+		const nextStep = this.steps[this.currStep];
+		if (nextStep.func){
+			nextStep.func();
+		} else if (nextStep.loop){
+			this.currStep = this.loopBeginMark - 1;
+		}
+	}
+
+	update(){
+		this.currDelay++;
+
+		// while (this.currStep < this.steps.length){
+			const nextStep = this.steps[this.currStep];
+			const nextDelay = nextStep.delay ? nextStep.delay : 0;
+			if (this.currDelay >= nextDelay){
+
+				this.executeNextStep();
+
+				this.currStep++;
+				this.currDelay = 0;
+			}
+		// }
+	}
+}
+
